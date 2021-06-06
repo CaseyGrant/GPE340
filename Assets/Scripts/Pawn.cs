25943420de6e5e169912182ddc97616e55b3aa67
Casey Grant <casgrant@uat.edu>
2021-06-06 11:49:32 -0700

    Completed Project 1
    Added root animation character with animations made a test level and made the pawn shoot projectiles

---
+++ Assets/Scripts/Pawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class Pawn : MonoBehaviour
+{
+    private Animator Anim; // holds an animator
+    public GameObject BulletSpawn; // holds the bullets spawn
+
+    [SerializeField] private float PawnSpeed; // sets the pawns speed
+    [SerializeField] private float BulletSpeed; // sets the bullets speed
+
+    void Start()
+    {
+        Anim = GetComponent<Animator>(); // gets the animator
+    }
+
+    void Update()
+    {
+        Vector3 MoveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); // gets the player input
+        MoveDirection = Vector3.ClampMagnitude(MoveDirection, 1); // makes the max value 1
+        Vector3 AnimationDirection = transform.InverseTransformDirection(MoveDirection); // makes North always north regardless of rotation
+
+        Anim.SetFloat("Speed", AnimationDirection.z * PawnSpeed); // sets the speed
+        Anim.SetFloat("Direction", AnimationDirection.x * PawnSpeed); // sets the direction
+
+        if(Input.GetKeyDown(KeyCode.Space)) // when the player hits the space key
+        {
+            BulletSpawn.GetComponent<SpawnProjectile>().Shoot(BulletSpeed); // spawns a bullet
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnProjectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProjectile : MonoBehaviour
+{
+    public GameObject Projectile; // holds the projectile
+
+    public void Shoot(float BulletSpeed)
+    {
+        GameObject Bullet = Instantiate(Projectile, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity); // creates a bullet
+        Bullet.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * BulletSpeed); // makes the bullet move
+    }
+}
