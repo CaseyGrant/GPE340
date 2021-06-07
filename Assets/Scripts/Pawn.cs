c90ef953532b4a94e1e1c2ee71a89216c18442d7
Casey Grant <casgrant@uat.edu>
2021-06-07 16:39:35 -0700

    Refactored

--- Assets/Scripts/Pawn.cs
+++ Assets/Scripts/Pawn.cs
@@ -1,21 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Animations;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Pawn : MonoBehaviour
 {
     private Animator Anim; // holds an animator
+
+    [Header("Pawn Settings")]
+    [SerializeField, Tooltip("Sets the speed the pawn can move"), Range(1f, 10f)]
+    private float PawnSpeed; // sets the pawns speed
+
+    [Header("Bullet Settings")]
+    [Tooltip("Sets where the bullets will spawn")]
     public GameObject BulletSpawn; // holds the bullets spawn
 
-    [SerializeField] private float PawnSpeed; // sets the pawns speed
-    [SerializeField] private float BulletSpeed; // sets the bullets speed
+    [SerializeField, Tooltip("Sets the speed the bullet can move"), Range(1f, 25f)]
+    private float BulletSpeed; // sets the bullets speed
 
+    /// <summary>
+    /// On start gets the animator and allows it to be accessed
+    /// </summary>
     void Start()
     {
         Anim = GetComponent<Animator>(); // gets the animator
     }
 
+    /// <summary>
+    /// Every frame update the animation parameters
+    /// Also check if space has been pressed then spawn a bullet
+    /// </summary>
     void Update()
     {
         Vector3 MoveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); // gets the player input
